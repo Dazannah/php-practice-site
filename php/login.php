7a45ecb24174773b6b26578195bde28a06741462
Dazannah <beattius@gmail.com>
2023-09-24 16:01:04 +0200

    changed userInput variable name, and now saving User object in sesson, to use in the future

--- php/login.php
+++ php/login.php
@@ -8,18 +8,19 @@ if(!isset($_POST["username"], $_POST["password"])){
   exit();
 }
 
-$user = (object) array(
+$userInputs = (object) array(
   "username" => $_POST["username"],
   "password" => $_POST["password"]
 );
 
-$loginUser = new LoginUser($user);
+$loginUser = new LoginUser($userInputs);
 $fetchedData = $loginUser->loginProcess();
+
+$user = new User($fetchedData['username'], $fetchedData['email']);
 
 session_start();
 
-$_SESSION["username"] = $fetchedData['username'];
-$_SESSION["email"] = $fetchedData['email'];
+$_SESSION["user"] = $user;
 header('Content-Type: application/json');
 
 echo json_encode(array("status" => true));
