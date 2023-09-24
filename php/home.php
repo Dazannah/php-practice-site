f7aadf2506ad1f474d372996ea942dee1936f914
Dazannah <beattius@gmail.com>
2023-09-24 16:01:42 +0200

    retrieving User infos from User object that stord in sessino

--- php/home.php
+++ php/home.php
@@ -1,13 +1,19 @@
 <?php 
+require_once("./module/User.php");
+
 session_start();
 
-if(!isset($_SESSION['username'])){
+if(!isset($_SESSION['user'])){
   header('Location: /php-practice-site/index.html');
 }
 
+$user = $_SESSION['user'];
+$username = $user -> username();
+$email = $user -> email();
+
 echo <<<EOT
-You are loged in as $_SESSION[username] <br>
-And your email is: $_SESSION[email]
+You are loged in as $username<br>
+And your email is: $email
 <br><br>
 <button onclick="window.location.href = '/php-practice-site/php/logout.php'">Logout</button>
 EOT;
